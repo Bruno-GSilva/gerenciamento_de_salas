9246ecf779002a7612cc74e0d8bc5c2506288adc
bgplaylab <bgplaylab@gmail.com>
2025-04-04 08:13:44 -0300

    init

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
-echo "<h1>Olá, mundo!</h1>";
-echo "<p>Esta é uma página PHP hospedada na Render 🚀</p>";
+$host = 'aws-0-sa-east-1.pooler.supabase.com';
+$db   = 'postgres';
+$user = 'postgres.jtquvxqbkvnakjmpivlw';
+$pass = '91Db113289*';
+$port = '6543';
+
+try {
+    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$db", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "<h1>Conectado com sucesso ao Supabase 🎉</h1>";
+} catch (PDOException $e) {
+    echo "Erro ao conectar: " . $e->getMessage();
+}
 ?>
