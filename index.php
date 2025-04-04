ca65d54399f4c47c824efdd80c806fd288a523ae
bgplaylab <bgplaylab@gmail.com>
2025-04-04 08:54:59 -0300

    init

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-$host = getenv('DB_HOST');
-$db   = getenv('DB_NAME');
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASSWORD');
-$port = getenv('DB_PORT') ?: '5432';
+$host = getenv('host');
+$db   = getenv('dbname');
+$user = getenv('user');
+$pass = getenv('password');
+$port = getenv('port');
 
 try {
     $dsn = "pgsql:host=$host;port=$port;dbname=$db";
