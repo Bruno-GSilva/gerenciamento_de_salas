a2927cb3b8a1603c65be45649debe985bd2440d8
Bruno-GSilva <brunogomesdsilva8@gmail.com>
2025-04-10 18:44:29 -0300

    teste

--- index.php
+++ index.php
@@ -9,9 +9,7 @@ try {
     $dsn = "pgsql:host=$host;port=$port;dbname=$db";
     $pdo = new PDO($dsn, $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "<h1>✅ Conectado ao Supabase com sucesso!</h1>";
 } catch (PDOException $e) {
-    echo "<h1>❌ Erro na conexão</h1>";
     echo "<pre>" . $e->getMessage() . "</pre>";
 }
 ?>
