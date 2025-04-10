fe985ebd9bd2c050087351fd6db6acbbda3d581f
Bruno-GSilva <brunogomesdsilva8@gmail.com>
2025-04-10 19:22:46 -0300

    teste2

--- cadastro.php
+++ cadastro.php
@@ -13,12 +13,13 @@ try {
     echo "<pre>" . $e->getMessage() . "</pre>";
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Hiran Tecnologia</title>
+  <title>Bruno Projeto</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="d-flex justify-content-center align-items-center vh-100 bg-light">
@@ -26,16 +27,16 @@ try {
     <h2 class="text-center mb-4">Cadastrar Usuario</h2>
     <form id="loginForm">
       <div class="mb-3">
-        <label for="login" class="form-label">Login</label>
-        <input type="login" class="form-control" id="login" placeholder="Digite seu login">
+        <label for="login2" class="form-label">Login</label>
+        <input type="login2" class="form-control" id="login2" placeholder="Digite seu login">
       </div>
       <div class="mb-3">
-        <label for="senha" class="form-label">Senha</label>
-        <input type="senha" class="form-control" id="senha" placeholder="Digite sua senha">
+        <label for="senha2" class="form-label">Senha</label>
+        <input type="senha2" class="form-control" id="senha2" placeholder="Digite sua senha">
       </div>
       <button type="submit" class="btn btn-primary w-100" onclick="validarCadastro()">Cadastrar</button>
-      <p id="mensagemErro" class="text-center mt-3 text-danger"></p>
-      <p id="mensagemOK" class="text-center mt-3 text-success"></p>
+      <p id="mensagemErro2" class="text-center mt-3 text-danger"></p>
+      <p id="mensagemOK2" class="text-center mt-3 text-success"></p>
     </form>
   </div>
 
@@ -47,11 +48,11 @@ try {
     });
 
     function validarLogin() {
-      document.getElementById("mensagemErro").style.display = "none";
-      document.getElementById("mensagemOK").style.display = "none";
+      document.getElementById("mensagemErro2").style.display = "none";
+      document.getElementById("mensagemOK2").style.display = "none";
 
-      let vLogin = document.getElementById("login").value;
-      let vSenha = document.getElementById("senha").value;
+      let vLogin = document.getElementById("login2").value;
+      let vSenha = document.getElementById("senha2").value;
 
       if (vLogin.trim() === "") {
         document.getElementById("mensagemErro").style.display = "block";
